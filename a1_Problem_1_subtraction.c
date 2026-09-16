15b7eab96d4369dd9310eb3aa6007919e94efaee
Pushkar Raj <pushkar.1974.78@gmail.com>
2026-09-16 17:20:03 +0530

    Create a1_Problem_1_subtraction.c

---
+++ a1_Problem_1_subtraction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a , b , sub;
+    printf("Enter the value of a and b\n");
+    scanf("%d\n %d",&a , &b);
+    sub = a - b;
+    printf("Answer is %d", sub);
+
+    return 0;
+}
