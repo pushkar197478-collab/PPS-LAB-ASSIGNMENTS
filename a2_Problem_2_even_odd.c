2982c0a6f7a62aba8cf4b9dda78fe0126fb96cd9
Pushkar Raj <pushkar.1974.78@gmail.com>
2026-09-16 17:21:07 +0530

    Create a2_Problem_2_even_odd.c

---
+++ a2_Problem_2_even_odd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter the value of num\n");
+    scanf("%d", &num);
+    if (num % 2 == 0)
+    {printf("The Number is Even");}
+    else 
+    {printf("The Number is Odd");}
+    return 0;
+}
