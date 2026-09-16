20e605d25b40a7e352bc5c5f51978c38227cd980
Pushkar Raj <pushkar.1974.78@gmail.com>
2026-09-16 17:19:47 +0530

    Create a1_Problem_1_modulo.c

---
+++ a1_Problem_1_modulo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a , b , c;
+    printf("Enter the value of a and b\n");
+    scanf("%d\n %d",&a , &b);
+    c = a % b;
+    printf("Answer is %d", c);
+
+    return 0;
+}
